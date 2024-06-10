ccb2cb797b898967033d5e29ab250231b1976968
FonKnp <fonnfonk@gmail.com>
2024-06-10 19:44:46 -0400

    thought controller update

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -4,7 +4,7 @@ module.exports = {
   // get all thoughts
   async getThought(req, res) {
     try {
-      const thoughts = await Thought.find().populate('thoughts');
+      const thoughts = await Thought.find();
       res.json(thoughts);
     } catch (err) {
       res.status(500).json(err);
@@ -13,8 +13,7 @@ module.exports = {
   // Get thought by ID
   async getThoughtById(req, res) {
     try {
-      const thought = await Thought.findOne({ _id: req.params.thoughtId })
-        .populate('thoughts');
+      const thought = await Thought.findOne({ _id: req.params.thoughtId });
       
       if (!thought) {
         return res.status(404).json({ message: 'No thought with that ID' });
