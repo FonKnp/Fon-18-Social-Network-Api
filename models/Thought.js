ee4096a8943a506cc4b08ef8498b661b4783d85d
FonKnp <fonnfonk@gmail.com>
2024-06-10 19:05:02 -0400

    update thought models

--- models/Thought.js
+++ models/Thought.js
@@ -1,37 +1,5 @@
 const { Schema, model } = require('mongoose');
-const ObjectId = require('mongodb').ObjectId;
-
-const formatedDate = (date) => {
-  return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-};
-
-const reactionSchema = new Schema(
-  {
-    reactionId: {
-      type: Schema.Types.ObjectId,
-      default: () => new ObjectId(),
-    },
-    reactionBody: {
-      type: String,
-      required: true,
-      maxLength: 280,
-    },
-    username: {
-      type: String,
-      required: true,
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-      get: formatedDate,
-    },
-  },
-  {
-    toJSON: {
-      getters: true,
-    },
-  },
-);
+const reactionSchema = require('./Reaction');
 
 const thoughtSchema = new Schema(
   {
